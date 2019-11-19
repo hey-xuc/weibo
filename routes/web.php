a2bd4e7af0c8cde2ae0479102e01fa4344226d2d
hey-xuc <13242087366@163.com>
2019-11-19 11:14:57 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 # 用户注册
 Route::get('/signup', 'UsersController@create')->name('signup');
+
+Route::resource('users', 'UsersController');
