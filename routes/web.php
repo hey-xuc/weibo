370d47ca7562328ad79ee9b8ca90bade547aea7d
hey-xuc <13242087366@163.com>
2019-11-22 02:53:25 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,9 @@ Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 
 # 验证邮箱
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+# 重设密码
+Route::get('password/reset', 'Auth/ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth/ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}','Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
